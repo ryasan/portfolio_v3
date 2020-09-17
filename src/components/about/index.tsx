37a07a84dc2c58f9b7ecba03b16531cd7c23ac75
Ryan Santos <ryansantos86@gmail.com>
2020-09-17 12:29:35 -0700

    progress: updated text

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -17,27 +17,27 @@ const AboutComponent: React.FC<AboutInterface> = ({ classNames }) => {
                             <h1 className='about__title'>About</h1>
                             <div className='about__paragraph-container'>
                                 <p className='about__paragraph'>
-                                    Lorem ipsum dolor sit amet consectetur,
-                                    adipisicing elit. Magni sit ratione vitae
-                                    repudiandae, nam minus, non officiis
-                                    nesciunt ipsa temporibus et illum?
-                                    Asperiores sapiente laudantium dolorum vero?
-                                    Deleniti, dolor! Nemo?
+                                    I haven't always been interested in
+                                    software. I didn't think I was smart enough
+                                    to code. I started out doing help desk and
+                                    support type roles. But as time went on I
+                                    was exposed more and more to the industry
+                                    and I found a passion in it. Now I love all
+                                    things web development & design!
                                 </p>
                                 <p className='about__paragraph'>
-                                    Lorem ipsum dolor sit amet consectetur,
-                                    adipisicing elit. Magni sit ratione vitae
-                                    repudiandae, nam minus, non officiis
-                                    nesciunt ipsa temporibus et illum?
-                                    Asperiores sapiente laudantium dolorum vero?
-                                    Deleniti, dolor! Nemo?
+                                    When I'm not on my never ending quest to
+                                    improve my coding techniques you'll find me
+                                    relaxing with one or two of my other hobbies
+                                    - billiards, skating, eating, or taking care
+                                    of my 2 little ones, a french bulldog & chihuahua :D
                                 </p>
                             </div>
                         </div>
                         <div className='about__column about__column--right'>
-                          <div className='about__illustration'>
-                              <SvgImage name='me' className='about__me' />
-                          </div>
+                            <div className='about__illustration'>
+                                <SvgImage name='me' className='about__me' />
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -7,46 +7,50 @@ interface SkillsInterface {
     classNames: string
 }
 
-const SkillsComponent: React.FC<SkillsInterface> = ({ classNames }) => {
-    return (
-        <section className={`skills ${classNames}`}>
-            <div className='parallax-wrapper'>
-                <div className='skills__outer'>
-                    <div className='skills__inner'>
-                        <div className='skills__column skills__column--left'>
-                            <div className='skills__tag-cloud-container'>
-                                <TagCloud />
-                            </div>
+const SkillsComponent: React.FC<SkillsInterface> = ({ classNames }) => (
+    <section className={`skills ${classNames}`}>
+        <div className='parallax-wrapper'>
+            <div className='skills__outer'>
+                <div className='skills__inner'>
+                    <div className='skills__column skills__column--left'>
+                        <div className='skills__tag-cloud-container'>
+                            <TagCloud />
                         </div>
-                        <div className='skills__column skills__column--right'>
-                            <h1 className='skills__title'>
-                                Skills &<br />
-                                Experience
-                            </h1>
-                            <div className='skills__text-container'>
-                                <p className='skills__paragraph'>
-                                    Lorem ipsum dolor sit amet consectetur,
-                                    adipisicing elit. Magni sit ratione vitae
-                                    repudiandae, nam minus.
-                                </p>
-                                <p className='skills__paragraph'>
-                                    Magni ratione vitae repudiandae, nam minus,
-                                    non officiis nesciunt ipsa temporibus et
-                                    illum? Asperiores sapiente laudantium
-                                    dolorum vero? Deleniti, dolor! Nemo?
-                                </p>
-                                <p className='skills__paragraph'>
-                                    Lorem ipsum dolor sit amet consectetur,
-                                    adipisicing elit. Magni sit ratione vitae
-                                    repudiandae.
-                                </p>
-                            </div>
+                    </div>
+                    <div className='skills__column skills__column--right'>
+                        <h1 className='skills__title'>
+                            Skills &<br />
+                            Experience
+                        </h1>
+                        <div className='skills__text-container'>
+                            <p className='skills__paragraph'>
+                                At the time of writing this my primary skills
+                                fall mostly under the frontend spectrum of the
+                                tech variety. However, I have experience in all
+                                areas of the stack and continue to learn the
+                                frontend, backend, and everything in between.
+                            </p>
+                            <p className='skills__paragraph'>
+                                My most recent projects have involved
+                                technologies like React, GraphQL, Prisma, CSS
+                                Tailwind, and D3. At the moment
+                                I'm pretty huge on GraphQL and believe it is the
+                                future of REST if it isn't already.
+                            </p>
+                            <p className='skills__paragraph'>
+                                I've been part of teams employing scrum
+                                methodologies, involved in the feature planning
+                                processes, Git rebase and merge workflows...the
+                                list goes on. If ever I have the opportunity to
+                                work with you, you'll quickly find that I give
+                                everything my all and put forth max effort.
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
-        </section>
-    )
-}
+        </div>
+    </section>
+)
 
 export default SkillsComponent
