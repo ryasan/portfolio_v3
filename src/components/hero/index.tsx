18294d5300d2902d814806961867e061944201cb
Ryan Santos <ryansantos86@gmail.com>
2020-10-03 18:07:48 -0700

    progress: added type animation to hero title

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useRef, useState, useEffect, RefObject } from 'react'
+import Typed from 'typed.js'
 
 import WorkstationIllustration from './workstation'
 import { components } from '../../pages'
+import { sleep } from '../../utils'
 import './hero.scss'
 
 interface HeroInterface {
@@ -12,42 +14,90 @@ interface HeroInterface {
 const HeroComponent: React.FC<HeroInterface> = ({
     classNames,
     handlePageClick
-}) => (
-    <section className={`hero ${classNames}`}>
-        <div className='parallax-wrapper'>
-            <div className='hero__outer'>
-                <div className='hero__inner'>
-                    <div className='hero__column hero__column--left'>
-                        <div className='hero__svg-container'>
-                            <WorkstationIllustration />
+}) => {
+    const refA = useRef<HTMLSpanElement>(null)
+    const refB = useRef<HTMLSpanElement>(null)
+    const [typedA, setTypedA] = useState<any>(null)
+    const [typedB, setTypedB] = useState<any>(null)
+
+    const sleepThenRemoveCursor = () => {
+        sleep(2000).then(() => removeCursor(refB))
+    }
+
+    const removeCursor = (ref: RefObject<HTMLSpanElement>) => {
+        ref?.current?.nextSibling?.remove()
+    }
+
+    const startB = () => {
+        removeCursor(refA)
+        setTypedB(
+            new Typed(refB.current, {
+                strings: ['Ryan'],
+                typeSpeed: 50,
+                backSpeed: 50,
+                onComplete: sleepThenRemoveCursor
+            })
+        )
+    }
+
+    useEffect(() => {
+        if (refA) {
+            setTypedA(
+                new Typed(refA.current, {
+                    strings: ['Hello Moto', 'My name is'],
+                    typeSpeed: 50,
+                    backSpeed: 50,
+                    startDelay: 3500,
+                    fadeOut: true,
+                    onComplete: startB
+                })
+            )
+        }
+    }, [refA])
+
+    useEffect(() => {
+        return () => {
+            typedA?.destroy()
+            typedB?.destroy()
+        }
+    }, [])
+
+    return (
+        <section className={`hero ${classNames}`}>
+            <div className='parallax-wrapper'>
+                <div className='hero__outer'>
+                    <div className='hero__inner'>
+                        <div className='hero__column hero__column--left'>
+                            <div className='hero__svg-container'>
+                                <WorkstationIllustration />
+                            </div>
                         </div>
-                    </div>
-                    <div className='hero__column hero__column--right'>
-                        <div className='hero__text-container'>
-                            <h1 className='hero__title'>
-                                Hello Moto
-                                <span className='hero__title-newline'>
-                                    My name is{' '}
-                                    <span className='hero__name'>Ryan</span>
-                                </span>
-                            </h1>
-                            <span className='hero__title-ampersand'>&</span>
-                            <h4 className='hero__subtitle'>
-                                I'm a frontend developer.
-                            </h4>
+                        <div className='hero__column hero__column--right'>
+                            <div className='hero__text-container'>
+                                <span
+                                    className='hero__title hero__title--1'
+                                    ref={refA}></span>
+                                <span
+                                    className='hero__title hero__title--2'
+                                    ref={refB}></span>
+                                <span className='hero__title-ampersand'>&</span>
+                                <h4 className='hero__subtitle'>
+                                    I'm a frontend developer.
+                                </h4>
+                            </div>
+                            <button
+                                className='hero__btn'
+                                onClick={() =>
+                                    handlePageClick(components.length - 1)
+                                }>
+                                CONTACT
+                            </button>
                         </div>
-                        <button
-                            className='hero__btn'
-                            onClick={() =>
-                                handlePageClick(components.length - 1)
-                            }>
-                            CONTACT
-                        </button>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-)
+        </section>
+    )
+}
 
 export default HeroComponent
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,6 +2,7 @@
 declare module 'react-helmet'
 declare module 'react-google-maps'
 declare module 'recompose'
+declare module 'typed.js'
 
 // assets
 declare module '*.svg'
